183fa190493f65df2567a924be47ab69b4aa1673
DragonStramp <54375110+DragonStramp@users.noreply.github.com>
2020-05-30 14:49:26 -0600

    +stuff

---
+++ js/index.js
@@ -0,0 +1,7 @@
+// var ship = document.getElementById("nav-image");
+// function shipOn() {
+//   ship.src = ("images/ship/ship-flame.png");
+// }
+// function shipOff() {
+//   ship.src = ("images/ship/ship.png");
+// }
